01510b221c4bf000a99d45c229f5937bc5428cba
Silent <zdanio95@gmail.com>
2020-05-21 19:27:31 +0200

    First commit

---
+++ source/VersionInfo.lua
@@ -0,0 +1,8 @@
+defines { 
+    "rsc_FullName=\"SilentPatch for Mafia 2: Definitive Edition\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2020\"",
+    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchM2DE/releases\""
+}
